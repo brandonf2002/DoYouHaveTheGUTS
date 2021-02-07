d250ce94c613e6de724ab4529d8075f44ab49edd
brandonf2002 <45246016+brandonf2002@users.noreply.github.com>
2021-02-07 17:02:52 +0000

    Add files via upload

---
+++ wrldApp.js
@@ -0,0 +1,141 @@
+var fs = require("fs");
+var http = require("http");
+var url = require("url")
+var Twit = require('twit')
+var config = require('./config')
+
+var T = new Twit(config)
+
+//Set up the natural API stuff
+var Analyzer = require('natural').SentimentAnalyzer;
+var stemmer = require('natural').PorterStemmer;
+
+//Initialise the sentiment analysis stuff
+var afinn = new Analyzer("English", stemmer, "afinn");
+var senticon = new Analyzer("English", stemmer, "senticon");
+var pattern = new Analyzer("English", stemmer, "pattern");
+
+//return how long ago the tweet was made in a readable way
+function calculateSince(datetime) {
+    //create new date objects
+    var tweetTime = new Date(datetime);
+    var currentTime = new Date();
+
+    //get the time difference from when the tweet was made to now
+    var minutesAgo = Math.round((currentTime - tweetTime) / 60000);
+    if (minutesAgo == 0) { var output = 'just now'; }
+    else if (minutesAgo == 1) { var output = '1 minute ago'; }
+    else if (minutesAgo < 60) { var output = minutesAgo + ' minutes ago'; }
+    else if (minutesAgo < 120) { var output = 'about 1 hour ago'; }
+    else if (minutesAgo < 1440) { var output = 'about ' + Math.round(minutesAgo / 60) + ' hours ago'; }
+    else if (minutesAgo < 2880) { var output = '1 day ago'; }
+    else {var output = Math.round(minutesAgo / 1440); + ' days ago';}
+    return output;
+};
+
+var zoom;
+
+async function getTweets(x, y){
+    var promise = T.get('search/tweets', { q: ' ', count: 100, geocode: x+","+ y +","+zoom+"mi"})
+
+    return promise.then(
+        function(result){
+            var fineTweets = []
+            var data = result.data
+            try {
+                for (var i = 0; i < data.statuses.length; i++) {
+                    //check if geolocation data is given in the tweet
+                    if (data.statuses[i].geo != null) {
+                        var tweet = data.statuses[i].text
+                        var id =  data.statuses[i].id
+    
+                        var a = tweet.toString();
+                        //strip any newlines in the tweet
+                        a = a.replace(/(\r\n|\n|\r)/gm, "");
+                        //convert tweet into an array of words
+                        var b = a.split(" ");
+                        //console.log(b);
+                        //check the tweet is an object and the elements within are strings
+                        //console.log("Type of whole tweet array: " + typeof (b));
+                        //console.log("Type of element in array: " + typeof (b[1]));
+    
+                        //get the sentiment of the tweet using different vocabularies
+                        var sentA = afinn.getSentiment(b)
+                        var sentB = senticon.getSentiment(b)
+                        var sentC = pattern.getSentiment(b)
+                        var overallSent = (sentA + sentB + sentC) / 3;
+                        var sentSummary;
+    
+                        //group the sentiment and provide a word summary
+                        if (overallSent < -0.1) { sentSummary = "Strongly Negative"; }
+                        else if (overallSent < -0.02) { sentSummary = "Mildly Negative"; }
+                        else if (overallSent<0.02) { sentSummary = "Undefined";}
+                        else if (overallSent < 0.1) { sentSummary = "Mildly Positive"; }
+                        else { sentSummary = "Strongly Positive"; }
+                        //console.log("Sentiment: " + sentSummary + " (" + overallSent + ")");
+                        fineTweets.push(JSON.stringify({"id":id,"text":tweet ,"sentSummary":sentSummary ,"overallSent":overallSent,"geo":data.statuses[i].geo.coordinates,"age":calculateSince(data.statuses[i].created_at)}))
+                    }   
+                }
+            } catch (err) {
+                console.log("No tweets in this area")
+                console.log(err)
+            }
+
+            return fineTweets.toString()
+        })
+}
+  
+var xy;
+
+//creates a http server using node to host the website
+app = http.createServer(async function (request, response) {
+
+    //handeling the post requests
+    if (request.method == 'POST') {
+        console.log("POST");
+        var body = '';
+        request.once('data', function (data) {
+
+            body += data;
+            data = JSON.parse(body)
+            xy = [data["lat"], data["lng"]]
+            zoom = data["zoom"]
+            zoom = (50/Math.pow(zoom, 1.3)).toString();
+            
+        });
+
+        response.writeHead(200, {'Content-Type': 'text/html'}); 
+        response.end('callback(\'{\"msg\": \"OK\"}\')');
+    
+    //handles the get requests
+    } else {
+
+    var pathname = url.parse(request.url).pathname;
+
+    console.log("Request for " + pathname + " received.");
+
+    response.writeHead(200);
+
+    //sends the tweets to the client
+    if(pathname == "/getTweets") {
+        var tweets = await getTweets(xy[0], xy[1])
+        response.write(tweets)
+    } 
+    else if(pathname == "/") {
+        html = fs.readFileSync("./index.html", "utf8");
+        response.write(html);
+    } 
+    else if (pathname == "/wrldEmbedded.js") {
+        script = fs.readFileSync("./wrldEmbedded.js", "utf8");
+        response.write(script);
+    }
+
+}
+
+	response.end();
+});
+
+//opens the server up to port 8000
+app.listen(8000)
+
+console.log("Listening to server on 8000...");
---
+++ wrldEmbedded.js
@@ -0,0 +1,108 @@
+var map = L.Wrld.map("map", "60b82eeb01681e0bbe79eb694d3f708c", {
+	center: [34.08699834538547, -118.34464786483618],
+	zoom: 15
+});
+
+var markers = []
+var ids = []
+iconDims = new L.Point(60, 100)
+var POIicon = {
+	"Strongly Negative" : new L.Icon({
+		iconUrl:"https://cdn.discordapp.com/attachments/807943427873701908/807943517124821052/poi_red.png",
+		iconSize: iconDims,
+	}),
+	"Mildly Negative" : new L.Icon({
+		iconUrl:"https://cdn.discordapp.com/attachments/807943427873701908/807943519288164413/poi_yellow.png",
+		iconSize: iconDims,
+	}),
+	"Undefined" : new L.Icon({
+		iconUrl:"https://cdn.discordapp.com/attachments/807943427873701908/807943509474541588/poi_grey.png",
+		iconSize: iconDims,
+	}),
+	"Mildly Positive" : new L.Icon({
+		iconUrl:"https://cdn.discordapp.com/attachments/807943427873701908/807943510165553152/poi_light_green.png",
+		iconSize: iconDims,
+	}),
+	"Strongly Positive" : new L.Icon({
+		iconUrl:"https://cdn.discordapp.com/attachments/807943427873701908/807943507897614376/poi_green.png",
+		iconSize: iconDims,
+	})
+}
+
+//function to place the marker at (x, y) cords with relivant info
+function placeMarker(x, y, text, id, age, sentiment){
+	var marker = L.marker([x, y], {
+		title: text + '\n--' + age,
+		elevation: 50
+	})
+	if (!ids.includes(id)){
+		marker.setIcon(POIicon[sentiment]);
+		ids.push(id)
+		marker.bindPopup(marker.options.title).openPopup();
+		marker.addTo(map);
+		markers.push(marker);
+	}
+}
+
+//pulls the scrapped tweets from the server to the front end
+async function getTweets(event) {
+	var xmlhttp = new XMLHttpRequest(); 
+	xmlhttp.open("POST", "/json-handler");
+	xmlhttp.setRequestHeader("Content-Type", "application/json");
+	xmlhttp.send(JSON.stringify({"lat":event.latlng["lat"], "lng":event.latlng["lng"], "zoom":map.getZoom()}));
+
+	var xhttp = new XMLHttpRequest();
+
+	//creating a Promise as the process can take upwards or a few hundred milliseconds, want to ensure the data stays whole and uncorrupted
+	var p = new Promise(async function (resolve, reject) {
+		xhttp.onreadystatechange = async function() {
+			if (this.readyState == 4 && this.status == 200) {
+				resolve(this.responseText)
+			}
+		};
+	})
+
+	xhttp.open("GET", "getTweets", true);
+	xhttp.send();
+
+	return p
+}
+
+//parsing the string of many tweets into a few json objects
+function parseTweets(tweets) {
+	tweets = tweets.substring(1, tweets.length-1);
+	tweets = tweets.split("},{"); 
+
+	tweetObjects = []
+
+	for (var i = 0; i < tweets.length; i++) {
+		tweets[i] = "{"+tweets[i]+"}"
+		tweetObjects.push(JSON.parse(tweets[i]))
+	}
+
+	return tweetObjects
+}
+
+//function to get, parse and place the tweets onto the map
+async function mapTweets(event) {
+	tweets = await getTweets(event)
+	tweets = parseTweets(tweets)
+	try {
+		for (var i = 0; i < tweets.length; i++) {
+			placeMarker(
+				tweets[i]["geo"][0], 
+				tweets[i]["geo"][1], 
+				tweets[i]["text"], 
+				tweets[i]["id"], 
+				tweets[i]["age"], 
+				tweets[i]["sentSummary"]
+			)
+		} 
+	} catch (err) {
+		//do nothing
+	}
+
+}
+
+//starts the proccess of scanning the nearby region for tweets
+map.on("click", mapTweets);
